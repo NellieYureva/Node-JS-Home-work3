ce802a9a8e3850a9862397ea0737364d4c669076
NellieYureva <152055885+NellieYureva@users.noreply.github.com>
2025-06-15 09:29:53 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,34 @@
 const express = require ('express');
-
-let countAbout = 0;
-let countMain = 0;
+const fs = require('fs');
+const path = require('path');
 
 const app = express();
 
 app.get('/', (req, res) => {
-    countMain++;
-    res.send(`<h1>Корневая страница</h1> <p>Просмотров: ${countMain}</p><a href="/about">Ссылка на страницу /about</a>`);
+    const pathToFileMain = path.join(__dirname, 'countMain.json');
+    const countMainData = JSON.parse(fs.readFileSync(pathToFileMain, 'utf-8'));
+
+    countMainData.count = countMainData.count + 1;
+
+    fs.writeFileSync(pathToFileMain, JSON.stringify(countMainData, null, 2));
+    res.send(`<h1>Корневая страница</h1> <p>Просмотров: ${countMainData.count}</p><a href="/about">Ссылка на страницу /about</a>`);
 });
 
 app.get('/about', (req, res) => {
-    countAbout++;
-    res.send(`<h1>Страница about</h1> <p>Просмотров: ${countAbout}</p><a href="/">Ссылка на корневую страницу /</a>`);
+    const pathToFileAbout = path.join(__dirname, 'countAbout.json');
+    const countAboutData = JSON.parse(fs.readFileSync(pathToFileAbout, 'utf-8'));
+
+    countAboutData.count = countAboutData.count + 1;
+
+    fs.writeFileSync(pathToFileAbout, JSON.stringify(countAboutData, null, 2));
+    res.send(`<h1>Страница about</h1> <p>Просмотров: ${countAboutData.count}</p><a href="/">Ссылка на корневую страницу /</a>`);
 });
 
-app.listen(3000);
+const port = 3000;
+
+app.listen(port, () => {
+    console.log(`Сервер запущен на порту ${port}`);
+});
 
 
 
